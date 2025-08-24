6ba54597ec059a93f0277ae59a473187027262d3
Tiago Poli <tiagoluizpoli@gmail.com>
2025-08-24 15:50:55 -0300

    refactor: adjust session repository

--- src/shared/database/repositories/drizzle-session.repository.ts
+++ src/shared/database/repositories/drizzle-session.repository.ts
@@ -1,16 +1,65 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DRIZLE_DB, type DrizzleDatabase } from '../drizzle-setup';
-import { Session } from 'src/core/domain';
-import { sessionsTable } from '../drizzle-setup/schema';
-import { eq } from 'drizzle-orm';
+import { Session, Tenant } from 'src/core/domain';
+import { sessionsTable, tenantsTable } from '../drizzle-setup/schema';
+import { and, eq } from 'drizzle-orm';
+
+export interface SessionsRepositoryGetAllParams {
+  tenantCode: string;
+}
+
+export interface SessionsRepositoryGetByIdParams {
+  tenantCode: string;
+  id: string;
+}
 
 @Injectable()
 export class DrizzleSessionRepository {
   constructor(@Inject(DRIZLE_DB) private readonly db: DrizzleDatabase) {}
 
-  async getById(id: string): Promise<Session | undefined> {
+  async getAll({
+    tenantCode,
+  }: SessionsRepositoryGetAllParams): Promise<Session[]> {
+    const sessions = await this.db.query.sessionsTable.findMany({
+      with: {
+        tenant: true,
+      },
+      where: eq(tenantsTable.code, tenantCode),
+    });
+
+    return sessions.map((session) =>
+      Session.create(
+        {
+          tenantId: session.tenantId,
+          customerId: session.customerId ?? undefined,
+          badgeId: session.badgeId,
+          startedAt: session.startedAt ?? undefined,
+          endedAt: session.endedAt ?? undefined,
+          tenant: Tenant.create(
+            {
+              code: session.tenant.code,
+              name: session.tenant.name,
+              createdAt: session.tenant.createdAt,
+              contactInfo: session.tenant.contactInfo ?? undefined,
+              description: session.tenant.description ?? undefined,
+            },
+            session.tenant.id,
+          ),
+        },
+        session.id,
+      ),
+    );
+  }
+
+  async getById({
+    tenantCode,
+    id,
+  }: SessionsRepositoryGetByIdParams): Promise<Session | undefined> {
     const session = await this.db.query.sessionsTable.findFirst({
-      where: eq(sessionsTable.id, id),
+      with: {
+        tenant: true,
+      },
+      where: and(eq(tenantsTable.code, tenantCode), eq(sessionsTable.id, id)),
     });
 
     if (!session) {
@@ -24,6 +73,16 @@ export class DrizzleSessionRepository {
         badgeId: session.badgeId,
         startedAt: session.startedAt ?? undefined,
         endedAt: session.endedAt ?? undefined,
+        tenant: Tenant.create(
+          {
+            code: session.tenant.code,
+            name: session.tenant.name,
+            createdAt: session.tenant.createdAt,
+            contactInfo: session.tenant.contactInfo ?? undefined,
+            description: session.tenant.description ?? undefined,
+          },
+          session.tenant.id,
+        ),
       },
       session.id,
     );
